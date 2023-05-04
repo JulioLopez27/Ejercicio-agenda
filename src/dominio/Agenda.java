f6e857c2bea1a9cba4a39751c178ed1907184adb
Julio Lopez <julioneo95@hotmail.com>
2023-05-04 11:59:39 -0300

    feature:Modifica logica de add contacto, se recibe los atributos y se valida / Implementa logica para devolver los contactos segun criterio de busqueda

--- src/dominio/Agenda.java
+++ src/dominio/Agenda.java
@@ -23,15 +23,48 @@ public class Agenda {
         return contactos.size();
     }
 
-    public boolean agregarContacto(Contacto contacto) {
-        if (contacto != null) {
-            contactos.add(contacto);
+    public boolean agregarContacto(TipoContacto tc, String nombre, String telefono) {
+        if (validar(nombre, telefono)) {
+            contactos.add(new Contacto(tc, nombre, telefono));
             return true;
         }
         return false;
     }
 
+    private boolean validar( String nombre, String telefono) {
+       if(nombre==null || nombre.trim().isEmpty() && telefono==null || telefono.trim().isEmpty())return false;
+       else return true;
+    }
+
     public void agregarContactoDatosPrueba(Contacto c) {
         contactos.add(c);
+    }
+
+    //pre:Cadena vacia muestro todos los contactos del usuario
+    public ArrayList<Contacto> obtenerContactos(String cadena) {
+        return cadena.isEmpty()
+                ? devolverTodosLosContactos()
+                : contactosContienenCadena(cadena);
+    }
+
+    private ArrayList<Contacto> devolverTodosLosContactos() {
+        ArrayList<Contacto> contactos = new ArrayList<>();
+        if (this.getContactos().size() == 0) {
+            return contactos;
+        }
+        for (Contacto contacto : this.getContactos()) {
+            contactos.add(contacto);
+        }
+        return contactos;
+    }
+
+    private ArrayList<Contacto> contactosContienenCadena(String cadena) {
+        ArrayList<Contacto> contactosADevolver = new ArrayList<>();
+        for (Contacto c : this.getContactos()) {
+            if (c.getNombre().contains(cadena) || c.getTelefono().contains(cadena)) {
+                contactosADevolver.add(c);
+            }
+        }
+        return contactosADevolver;
     }
 }
