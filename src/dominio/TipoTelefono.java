c272d7dc92cefb22ff3f08ba2d66bec5db08b2b8
Julio Lopez <julioneo95@hotmail.com>
2023-05-06 21:25:54 -0300

    feature: implementacion clase TipoTelefono(abstracta) con sus validaciones

---
+++ src/dominio/TipoTelefono.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dominio;
+
+/**
+ *
+ * @author Julio Cesar
+ */
+public abstract class TipoTelefono {
+    private String nombre;
+
+    public TipoTelefono(String nombre) {
+        this.nombre = nombre;
+    }
+public abstract boolean esValido(String numero);
+    
+    public boolean esNumerico(String texto){
+        //trata de transformar el string a valor numerico
+        Long numero;
+        try {
+            numero=Long.valueOf(texto);
+        } catch (NumberFormatException e) {
+            //no es string
+            return false;
+        }
+        return numero>=0;
+    }
+    
+}
