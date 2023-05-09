8ba92b970a69b9d9aaa7be344ff5336fb73ff506
Julio Lopez <julioneo95@hotmail.com>
2023-05-08 22:26:12 -0300

    feature: add ToString()

--- src/dominio/TipoTelefono.java
+++ src/dominio/TipoTelefono.java
@@ -9,27 +9,34 @@ package dominio;
  * @author Julio Cesar
  */
 public abstract class TipoTelefono {
+
     private String nombre;
 
     public TipoTelefono(String nombre) {
         this.nombre = nombre;
     }
-public abstract boolean esValido(String numero);
+
+    public abstract boolean esValido(String numero);
 
     public String getNombre() {
         return nombre;
     }
-    
-    public boolean esNumerico(String texto){
+
+    public boolean esNumerico(String texto) {
         //trata de transformar el string a valor numerico
         Long numero;
         try {
-            numero=Long.valueOf(texto);
+            numero = Long.valueOf(texto);
         } catch (NumberFormatException e) {
             //no es string
             return false;
         }
-        return numero>=0;
+        return numero >= 0;
     }
-    
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+
 }
