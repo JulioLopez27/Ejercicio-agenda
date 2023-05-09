d64137cba3085c6f53edd4f51ba52cb601654909
Julio Lopez <julioneo95@hotmail.com>
2023-05-08 22:31:25 -0300

    change: se elimina codigo de consola

--- src/main/AgendaConsola.java
+++ src/main/AgendaConsola.java
@@ -1,27 +1,12 @@
 package main;
 
-import dominio.Usuario;
-import interfazConsola.CrearContactoConsola;
-import interfazConsola.LoginConsola;
 import interfazSwing.IuEntrada;
-import interfazSwing.VentanaLogin;
-import utilidades.Consola;
+
 
 public class AgendaConsola {
 
     public static void main(String[] args) throws Exception {
         DatosPrueba.cargar();
-
-        //inicializo consola para login
-//        LoginConsola loginConsola = new LoginConsola();
-        //Recibo usuario o null
-//        Usuario user = loginConsola.loguearUsuario();
-//        if (user == null) {
-//            Consola.println("Datos no validos");
-//        } else {
-//            CrearContactoConsola contactoConsola = new CrearContactoConsola(user);
-//            contactoConsola.ejecutar();
-//        }
 //---------Logica para la interfaz con swing
         IuEntrada system = new IuEntrada();
         system.setVisible(true);
