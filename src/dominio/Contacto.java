730936ab4220d92af73769859005ed5a17a17fd7
Julio Lopez <julioneo95@hotmail.com>
2023-05-08 22:15:10 -0300

    feature: implementacion getters/setter y validaciones

--- src/dominio/Contacto.java
+++ src/dominio/Contacto.java
@@ -10,25 +10,17 @@ package dominio;
  */
 public class Contacto {
 
-    TipoContacto tipoContacto;
-    String nombre;
-    String telefono;
+    private TipoContacto tipoContacto;
+    private String nombre;
+    private Telefono telefono;
 
-    public Contacto(TipoContacto tipoContacto, String nombre, String telefono) {
+    public Contacto(TipoContacto tipoContacto, String nombre, Telefono telefono) {
         this.tipoContacto = tipoContacto;
         this.nombre = nombre;
         this.telefono = telefono;
     }
 
     public Contacto() {
-    }
-
-    public TipoContacto getTipoContacto() {
-        return tipoContacto;
-    }
-
-    public void setTipoContacto(TipoContacto tipoContacto) {
-        this.tipoContacto = tipoContacto;
     }
 
     public String getNombre() {
@@ -39,17 +31,49 @@ public class Contacto {
         this.nombre = nombre;
     }
 
-    public String getTelefono() {
+    public TipoContacto getTipoContacto() {
+        return tipoContacto;
+    }
+
+    public void setTipoContacto(TipoContacto tipoContacto) {
+        this.tipoContacto = tipoContacto;
+    }
+
+    public Telefono getTelefono() {
         return telefono;
     }
 
-    public void setTelefono(String telefono) {
+    public void setTelefono(Telefono telefono) {
         this.telefono = telefono;
     }
 
     @Override
     public String toString() {
         return "Nombre: " + nombre + ", Telefono: " + telefono + " ";
+    }
+
+    public boolean validar() {
+        return esNombreValido() && esTelefonoValido() && esTipoContactoValido();
+    }
+
+    private boolean esNombreValido() {
+        return this.getNombre() != null && !this.getNombre().trim().isEmpty();
+    }
+
+    private boolean esTelefonoValido() {
+        return this.getTelefono() != null && this.getTelefono().esValido();
+    }
+
+    private boolean esTipoContactoValido() {
+        return this.getTipoContacto() != null;
+    }
+
+    public boolean contiene(String criterio) {
+        if (this.getNombre().toUpperCase().contains(criterio.toUpperCase())
+                || this.getTelefono().contiene(criterio)) {
+            return true;
+        }
+        return false;
     }
 
 }
