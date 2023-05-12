4604bf026125e53fdf01c5e61ae36254baf8adec
Julio Lopez <julioneo95@hotmail.com>
2023-05-12 16:37:21 -0300

    feat: implementacion login para usuarios

--- src/servicios/ServicioUsuarios.java
+++ src/servicios/ServicioUsuarios.java
@@ -4,8 +4,11 @@
  */
 package servicios;
 
+import dominio.UsuarioAdministrador;
 import dominio.UsuarioAgenda;
+import dominio.UsuarioGenerico;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -13,41 +16,56 @@ import java.util.ArrayList;
  */
 public class ServicioUsuarios {
 
-    private ArrayList<UsuarioAgenda> usuarios = new ArrayList<>();
+    private List<UsuarioAgenda> usuariosAgenda;
+    private List<UsuarioAdministrador> usuariosAdministrador;
 
-    public UsuarioAgenda login(String username, String password) {
-        UsuarioAgenda user = null;
-        for (UsuarioAgenda usuario : usuarios) {
-
-            if (usuario.compararUsuarios(username, password)) {
-                user = usuario;
-            }
-        }
-
-        return user;
+    public ServicioUsuarios() {
+        usuariosAgenda = new ArrayList<>();
+        usuariosAdministrador = new ArrayList<>();
     }
 
-    private ArrayList<UsuarioAgenda> getUsuarios() {
-        return usuarios;
+    public UsuarioAgenda loginAgenda(String usuario, String password) {
+        return (UsuarioAgenda)this.loginUsuario(usuario, password, (ArrayList)usuariosAgenda);
+    }
+
+    public UsuarioAdministrador loginAdministrador(String usuario, String password) {
+        return (UsuarioAdministrador)this.loginUsuario(usuario, password, (ArrayList)usuariosAdministrador);
     }
 
     public boolean agregar(UsuarioAgenda user) {
 
-        if (user != null && !existeUsuario(user)) {
-            usuarios.add(user);
+        if (user != null) {
+            usuariosAgenda.add(user);
             return true;
         }
-
         return false;
     }
 
-    private boolean existeUsuario(UsuarioAgenda user) {
-        for (UsuarioAgenda usuario : usuarios) {
-            if (usuario.existeUsuario(user)) {
-                return true;
-            }
+    public boolean agregar(UsuarioAdministrador userAdmin) {
+        if (userAdmin != null) {
+            usuariosAdministrador.add(userAdmin);
+            return true;
         }
         return false;
     }
 
+    private UsuarioGenerico loginUsuario(String usuario, String password, List<UsuarioGenerico> listaUsuarios) {
+        for (UsuarioGenerico u : listaUsuarios) {
+            if (u.getNombre().equals(usuario) && u.esPasswordValida(password)) {
+                return u;
+            }
+        }
+        return null;
+    }
+
+    //TODO: reactivar codigo de ser necesario verificar si ya existe un usuairo con 
+    //datos iguales
+//    private boolean existeUsuario(UsuarioAgenda user) {
+//        for (UsuarioAgenda usuario : usuarios) {
+//            if (usuario.existeUsuario(user)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 }
