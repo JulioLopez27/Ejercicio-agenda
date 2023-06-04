9dad835ade07dcca820b2f7a4e729f81a037de37
Julio Lopez <julioneo95@hotmail.com>
2023-06-04 01:05:42 -0300

    Implementacion de metodos

--- src/observer/Observable.java
+++ src/observer/Observable.java
@@ -10,29 +10,30 @@ import java.util.ArrayList;
  *
  * @author Julio Cesar
  */
-public class Observable {
-      ArrayList<Observer> observadores= new ArrayList<>();
-    
-    
-    public enum Evento{
-       LISTA_SESIONES_ACTUALIZADA,
-       AGENDA_ACTUALIZADA,
+public abstract class Observable {
+
+    ArrayList<Observer> observadores;
+
+    public enum Evento {
+        LISTA_SESIONES_ACTUALIZADA,
+        AGENDA_ACTUALIZADA,
     }
 
     public Observable() {
+        observadores = new ArrayList<>();
     }
-    
-    public void suscribir(Observer o){
+
+    public void suscribir(Observer o) {
         this.observadores.add(o);
     }
-    
-    public void desuscribir(Observer o){
+
+    public void desuscribir(Observer o) {
         this.observadores.remove(o);
     }
-    
-    public void notificar(Observable.Evento e ){
+
+    public void notificar(Observable.Evento e) {
         for (Observer o : observadores) {
-            o.notificar(this,e);
+            o.notificar(this, e);
         }
     }
 }
